a5ad254241edc20f9349a5c78aed727e9ed81261
Michael <michael.dburke56@gmail.com>
2021-11-23 17:52:39 -0500

    Build function to create query string.

--- client/index.js
+++ client/index.js
@@ -1,5 +1,35 @@
+const createQueryString = (inputArray) => {
+  const leftOp = document.getElementById("leftOp").value;
+  const rightOp = document.getElementById("rightOp").value;
+  const radioAddition = document.getElementById("radioAddition").checked;
+  const radioSubtraction = document.getElementById("radioSubtraction").checked;
+  const radioMultiplication = document.getElementById(
+    "radioMultiplication"
+  ).checked;
+  const radioDivision = document.getElementById("radioDivision").checked;
+  const radioRemainder = document.getElementById("radioRemainder").checke;
+
+  let queryStr = `leftOperand=${leftOp}&rightOperand=${rightOp}&operation=`;
+
+  //Create error checking function to ensure multiple operators have not been selectes.
+  //Ensure left and right Operands are of correct type (Number).
+
+  if (radioAddition) {
+    queryStr += `+`;
+  }else if (radioSubtraction) {
+    queryStr += `-`;
+  }else if (radioMultiplication) {
+    queryStr += `*`;
+  }else if (radioDivision) {
+    queryStr += `/`;
+  }else if (radioRemainder) {
+    queryStr += `%`;
+  }
+
+  return queryStr;
+};
+
 const makeCalculation = () => {
-
-console.log(`external javascript file called.`)
-
-}
+  const queryString = createQueryString();
+  console.log(`query string:\n${queryString}`)
+};
