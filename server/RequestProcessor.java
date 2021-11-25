4355dc0025a7849dc1bf9b86cfe7138a54256dbe
Michael <michael.dburke56@gmail.com>
2021-11-25 14:46:33 -0500

    Handle operator as a char type value.

--- server/RequestProcessor.java
+++ server/RequestProcessor.java
@@ -1,7 +1,6 @@
 import java.io.*;
 import java.net.Socket;
 
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -31,51 +30,62 @@ public class RequestProcessor implements Runnable {
 		try {
 			incoming = in.readLine();
 			System.out.println(incoming);
-			//
+
 			String leftOp = "";
-		    boolean leftOpComplete = false;
-		    String rightOp = "";
-		    boolean rightOpComplete = false;
-		    char operation = 'x';
-		    
-		    for (int i = 17; i < incoming.length(); i++) {
-		    			
-		    
-		      //add char to leftOp
-		      if (!leftOpComplete && incoming.charAt(i) != '&'){
-		      leftOp += incoming.charAt(i);
-		      }
-		      
-		      //set left op as complete
-		      if (!leftOpComplete && incoming.charAt(i) == '&'){
-		      leftOpComplete = true;
-		      i += 14;
-		      }
-		      
-		      //add char to rightOp
-		      if (!rightOpComplete && leftOpComplete && incoming.charAt(i) != '&'){
-		      rightOp += incoming.charAt(i);
-		      }
-		      
-		      //Locate operation
-		      if (leftOpComplete && incoming.charAt(i) == '&'){
-		      rightOpComplete = true;
-		      operation = incoming.charAt(i + 11);
-		      }	      
-		    }  
-			
-		    System.out.println(leftOp);
-		    System.out.println(rightOp);
-		    System.out.println(operation); 
-		    
-			//
-			jsonObject.put("Expression", "1 + 2");
-			jsonObject.put("Result", "3");
+			boolean leftOpComplete = false;
+			String rightOp = "";
+			boolean rightOpComplete = false;
+			char operation = 'x';
+
+			for (int i = 17; i < incoming.length(); i++) {
+
+				// add char to leftOp
+				if (!leftOpComplete && incoming.charAt(i) != '&') {
+					leftOp += incoming.charAt(i);
+				}
+
+				// set left op as complete
+				if (!leftOpComplete && incoming.charAt(i) == '&') {
+					leftOpComplete = true;
+					i += 14;
+				}
+
+				// add char to rightOp
+				if (!rightOpComplete && leftOpComplete && incoming.charAt(i) != '&') {
+					rightOp += incoming.charAt(i);
+				}
+
+				// Locate operation
+				if (leftOpComplete && incoming.charAt(i) == '&') {
+					rightOpComplete = true;
+					operation = incoming.charAt(i + 11);
+				}
+			}
+
+			System.out.println(leftOp);
+			System.out.println(rightOp);
+			System.out.println(operation);
+
+			int total = 0;
+
+			if (operation == '+') {
+				total = Integer.parseInt(leftOp) + Integer.parseInt(rightOp);
+			} else if (operation == '-') {
+				total = Integer.parseInt(leftOp) - Integer.parseInt(rightOp);
+			} else if (operation == '*') {
+				total = Integer.parseInt(leftOp) * Integer.parseInt(rightOp);
+			} else if (operation == '/') {
+				total = Integer.parseInt(leftOp) / Integer.parseInt(rightOp);
+			} else if (operation == '%') {
+				total = Integer.parseInt(leftOp) % Integer.parseInt(rightOp);
+			}
+
+			jsonObject.put("Expression", leftOp + operation + rightOp);
+			jsonObject.put("Result", total);
 		} catch (IOException | JSONException e) {
 			e.printStackTrace();
 		}
 
-		// Implemented this try/catch block to resolve unhandled error, errors.
 		try {
 			String response = msgToClient + jsonObject.toString();
 			os.write(response.getBytes());
@@ -85,7 +95,5 @@ public class RequestProcessor implements Runnable {
 
 		}
 	}
-	
-	
-	
+
 }
